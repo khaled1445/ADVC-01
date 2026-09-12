c4834089a029f5bf12b008595d3d40303bec2036
5aled998 <khaledamr142005@.com>
2026-09-12 16:11:30 +0300

    Q1

--- ADVC#01/Program.cs
+++ ADVC#01/Program.cs
@@ -1,10 +1,98 @@
-﻿namespace ADVC_01
+﻿using System.Numerics;
+
+namespace ADVC_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1: What is a generic class? Why use generics?
+            // it's a class that can work with any data type (also can have a non generic data types). Generics allow you to create classes and methods that can operate on different types.
+            // By using generics, you can avoid code duplication and create reusable , easy maintainable components that can work with various data types without risking type safety.
+            #endregion
+
+            #region Q2: Write a generic class Container<T> with Add and Get methods.
+            // 
+
+            #endregion
+
+            #region  Q3: What are multiple type parameters? Write Pair<TKey, TValue>.
+            // 
+            #endregion
+
+            #region  Q4: What is a generic method? Write Swap < T > method.
+            // 
+            #endregion
+
+            #region  Q5: Write a generic method FindMax < T > that finds maximum value
+            //
+            #endregion
+
+            #region  Q6: What is a generic interface? Write IRepository<T>.
+            //
+            #endregion
+
+            #region  Q7: What is the 'struct' constraint? Write an example.
+            //
+            #endregion
+
+            #region  Q8: What is the 'class' constraint? Write an example.
+            //
+            #endregion
+
+            #region  Q9: What is the 'new()' constraint? Write an example. 
+            //
+            #endregion
+
+            #region  Q10: What is the interface constraint? Write an example.
+            //
+
+            #endregion
+
+            #region   Q11: What is the base class constraint? Write an example.
+            //
+
+            #endregion
+            #region  Q12: How do you apply multiple constraints? Write an example.
+            //
+            #endregion
+
+            #region  Q13: What does the 'default' keyword do in generics?
+            //
+            #endregion
+
+            #region  Q14: Write a SafeList < T > that returns default when the index is invalid.
+            //
+            #endregion
+
+            #region  Q15: What is covariance? Explain the 'out' keyword.
+            //
+
+            #endregion
+
+            #region  Q16: What is contravariance? Explain the 'in' keyword.
+            //
+
+            #endregion
+
+            #region  Q17: What is the difference between covariance and contravariance? 
+            //
+            #endregion
+
+            #region  Q18: How do static members work in generic types?
+            //
+
+            #endregion
+
+            #region Q19: How can you inherit from a generic class?
+            //
+            #endregion
+
+            #region Q20: Complete Exercise -Create a generic Cache < TKey, TValue>with Add, Get, Remove, Contains, and expiration support
+            //
+
+            #endregion
         }
+
     }
 }
