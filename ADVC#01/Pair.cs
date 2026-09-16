3eefab184557b05b38418701fdacc27c7b4ebcc2
5aled998 <khaledamr142005@.com>
2026-09-16 11:25:20 +0300

    Qquestion03

---
+++ ADVC#01/Pair.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADVC_01
+{
+    internal class Pair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+
+        public Pair(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
